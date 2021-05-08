1829418f0bd37b2c7b2d7989bf09aaa33223df78
guaigua <gerguaigua@gmail.com>
2021-05-08 11:10:37 -0300

    Criaçao CRUD students

--- toters-api/index.js
+++ toters-api/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const courses = require('./routes/courses');
+const todolists = require('./routes/todolists');
+const students = require('./routes/students');
 const app = express()
 app.set('view engine', 'ejs')
-
 
 const cors = require("cors")
 app.use(cors())
 app.use(express.json())
-app.use('/courses', courses);
+app.use('/', todolists);
+app.use('/students', students);
 
 
 
---
+++ toters-api/routes/students.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var router = express.Router();
+
+const { students } = require('../db');
+
+// List students
+router.get('/students', async (req, res) => {
+    const studentsList =  await students.findAll();
+    res.json({ action: 'Show Students ', students: studentsList })
+})
+
+// Show students for ID
+router.get('/students/:id', async  (req, res) => {
+    const studentsId = req.params.id
+    const studentsList =  await students.findByPk(studentsId);
+    res.json({ action: 'Show students for ID ', students: studentsList })
+})
+
+// Create one student
+router.post('/students', async (req, res) => { 
+    var errors=[]
+    if (!req.body.firstname){
+        errors.push("Nome não enviada");
+    }
+    if (!req.body.lastname){
+        errors.push("Sobrenome não enviada");
+    }
+    if (!req.body.mail){
+        errors.push("Mail não enviada");
+    }
+    // if (!req.body.birth){
+    //     errors.push("Dado do nascemento  não enviada");
+    // }
+    if (!req.body.country){
+        errors.push("País não enviada");
+    }
+    // if (!req.body.urlphoto){
+    //     errors.push("foto não enviada");
+    // }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    const studentsCreate = await students.create({             
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        mail: req.body.mail,
+        birth: req.body.birth,
+        country: req.body.country,
+        urlphoto: req.body.urlphoto,
+    })
+
+
+    console.log(studentsCreate);
+ 
+    res.json({ action: 'Students Create ', studentsCreate})
+})
+
+// update one students
+
+router.put('/students/:id', async (req, res) =>{
+    try{
+        const studentsId = req.params.id
+        const body = req.body
+        const studentsList = await students.findByPk(studentsId)
+        studentsList.update({
+            firstname: body.firstname,
+            lastname: body.lastname,
+            mail: body.mail,
+            birth: body.birth,
+            country: body.country,
+            urlphoto: body.urlphoto
+        });        
+        res.send({ action: 'Students Modify ', studentsList:studentsList })
+    } catch (e) {
+        console.log(e);
+        return res.send({ error: e})
+    }
+})
+
+// delete one students
+router.delete('/students/:id', async (req, res) => {
+    try{
+        const studentsId = req.params.id
+        const studentsRemove = await students.destroy({ where: { id: studentsId  } })
+        res.send({ action: 'students Remove ', studentsRemove: studentsRemove })
+    } catch (e) {
+        console.log(e);
+        return res.send({ error: e})
+    }
+})
+
+
+
+
+module.exports = router;
