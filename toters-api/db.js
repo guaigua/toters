f15cd6cfa5d03138881bec0d7730f3ceb01c6c29
Barbara Villegas <barbaraavillegas@gmail.com>
2021-05-10 19:21:51 -0300

    Atualizaçao BD

--- toters-api/db.js
+++ toters-api/db.js
@@ -4,11 +4,21 @@ const Task = require('./models/todolists')
 
 const Student = require('./models/students')
 
+const Teacher = require('./models/teachers')
+
+const Course = require('./models/courses')
+
 const sequelize = new Sequelize({ dialect: 'sqlite', storage: './toters.db' })
 
 const tasks = Task(sequelize, DataTypes)
 
 const students = Student(sequelize, DataTypes)
+
+const teachers = Teacher(sequelize, DataTypes)
+
+const courses = Course(sequelize, DataTypes)
+
+
 
 // sequelize.sync({ force: false }) 
 // .then(() => {
@@ -17,7 +27,9 @@ const students = Student(sequelize, DataTypes)
 
 module.exports ={
     tasks,
-    students    
+    students,
+    teachers, 
+    courses 
 }
 
 
